5a89385b9da884f932f290fdd885e1b395c514d6
manveerwalia <46795445+manveerwalia@users.noreply.github.com>
2019-01-17 17:01:56 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0727051
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dogs = new List<string>();
+            dogs.Add("Bulldog");
+            dogs.Add("Poodie");
+            dogs.Add("Retriever");
+            //foreach (var dog in dogs)
+            //{
+            //    Console.WriteLine(dog + "");
+            //}
+            //Console.ReadLine();
+            int[] a1 = new int[](1, 3, 5, 7, 9, 11);
+
+        foreach (int i in a1)
+            {
+                Console.WriteLine("the value of i is" + i);
+            }
+
+        }
+
+    }
+}
